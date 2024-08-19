5c15d987e895a5e62429aa1dd9fdbdc471750207
BonkAddict29 <tolpapiotr@proton.me>
2024-08-19 13:09:46 +0200

    Update binary encrypter.c

--- projects/ByteEncrypter/binary encrypter.c
+++ projects/ByteEncrypter/binary encrypter.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <conio.h>  
-
+//WARNING This is my first C program, the code is probably very carcinogenic. You have been warned.
 int main() {
     char input[1500];
     char bitPositions[100];
